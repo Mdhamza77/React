bb0fdaf9abfc3e0dc7f5729d0bccfbcc6752d0de
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-15 09:11:07 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+        <div>
+    <h1>Hello!</h1>
+    <h2>Good to see you here.</h2>
+  </div>
+    </div>
+  );
+}
+
+export default App;
