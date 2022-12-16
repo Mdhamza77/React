aeacc083ae19fe47d5c67d1bd251e4bdc3a238b1
Asplap3277 <mmohammedhamza333@gmail.com>
2022-12-16 14:12:16 +0530

    Implementation on various  Conditional Handler

--- src/Events.js
+++ src/Events.js
@@ -46,3 +46,29 @@ function Greeting(props){
         return <Guest/>
      }
 }
+
+
+// Inline Conditonals 
+
+
+function Mailbox(props) {
+    const unreadMessages = props.unreadMessages;
+    return (
+      <div>
+        <h1>Hello!</h1>
+        {unreadMessages.length > 0 &&
+          <h2>
+            You have {unreadMessages.length} unread messages.
+          </h2>
+        }
+      </div>
+    );
+  }
+  
+  const messages = ['React', 'Re: React', 'Re:Re: React'];
+  
+  const root = ReactDOM.createRoot(document.getElementById('root')); 
+  root.render(<Mailbox unreadMessages={messages} />);
+
+
+  //  
--- src/index.js
+++ src/index.js
@@ -1,33 +1,47 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import App from './App';
 import './index.css';
-import App from './App' ;
+//import App from './App' ;
 import reportWebVitals from './reportWebVitals';
-const root = ReactDOM.createRoot(document.getElementById('root'));
 
-root.render(<App/>)
+  const root = ReactDOM.createRoot(document.getElementById('root')); 
 
-function User(props){
-    return <h1>Welcome to Home page</h1>
-}
-function Guest(props){
-return <h1>Welcome guest , please signup</h1>
-}
+  function Warn(props){
+    if(!props.warn){
+      return null ;
+    }
+    return (
+      <div> 
+            Warning 
+      </div>
+    )
+  }
+  
+  class Prevent extends React.Component {
+    constructor(props) {
+      super(props); 
+      this.state = {show : true}
+      this.Toggle = this.Toggle.bind(this);
+    }
+     Toggle(){
+      this.setState(state =>({
+            show : !state.show 
+      }))
+     }
+     render()  {
+      return  (
+        <div> 
+            <Warn warn= {this.state.show} /> 
+            <button onClick = {this.Toggle}>{this.state.show ? 'Hide' : 'Show'}
+            </button>    
+        
+        </div>
+      )
+     }
+  }
 
-
-function Greeting(props){
-const isLogged = props.isLogged
-if(isLogged){
-return <User/>
-}
-else {
-return <Guest/>
-}
-}
-
-
-root.render(<Greeting  isLogged = {false} />)
-
+  root.render(<Prevent/>)
 
 console.log("Define React ?")
 // If you want to start measuring performance in your app, pass a function
