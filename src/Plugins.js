189dcd87a7c4e5c9cd812014e1246a5e63aee26e
Mdhamza77 <mmohammedhamza333@gmail.com>
2022-12-23 15:26:45 +0530

    Jquery, Spread and Jsx Exp

--- src/Plugins.js
+++ src/Plugins.js
@@ -3,19 +3,22 @@ import React from 'react';
 import $ from 'jquery'
 class Plugins extends React.Component {
     componentDidMount(){
+        
         this.$el = $(this.el);
         
     }
     componentWillUnmount(){
-        console.log("Exit the")
+        
+        alert("Done unmounting")
     }
     render() {
         return (
-            <div>
-                <select className='select' ref={el => this.el = el}>
-                    {this.props.children}
+            <>                
+            <select className='select' ref={el => this.el = el}>
+                {this.props.children}
                 </select>
-            </div>
+            </>
+
         );
     }
 }
